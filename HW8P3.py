4bec2ef1a5d29ef69ee5e964c0c8e5ca3ffc1b54
MatthewFletcher <bassmattf@gmail.com>
2017-10-12 20:32:20 -0500

    Working fibonacci sequence, final code

--- HW8P3.py
+++ HW8P3.py
@@ -13,16 +13,32 @@
 #    Class:  ENG101         	                                          #
 #  Helpers:  None                                                         #
 #                                                                         #
-#  Program:  HW Template                                                  #
+#  Program:  HW8P3                                                        #
 # Due Date:  16 Nov 2017                                                  #
 #                                                                         #
 # Language:  Python 3.5.1                                                 #
 #      IDE:  Python in Terminal                                           #
 #                                                                         #
-# Purpose:                                                                #
+# Purpose:   Create function to compute the Nth Fibonacci number          #
 #   "Bugs":                                                               #
 #   "Undocumented features": None.                                        #
 ###########################################################################
 
 
-#HW 6
+
+
+def fibN(n):
+	iteration = n
+	result = n
+	while iteration >1:
+		result = result * (iteration-1)
+		iteration = iteration - 1
+		#print('result = %i'%result)
+		#print('iteration = %i'%iteration)
+	return result
+
+
+#Get user choice for input
+choice = int(input("Enter number here >>> "))
+
+print('The %ith Fibonacci number is %i'%(choice,fibN(choice)))
