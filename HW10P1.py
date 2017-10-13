91edb75ba54d189d8dfdab8149a3edaa5505c1d3
MatthewFletcher <bassmattf@gmail.com>
2017-10-13 14:59:57 -0500

    All working except for final print

---
+++ HW10P1.py
@@ -0,0 +1,52 @@
+#    _     _      _     _      _     _      _     _      _     _      _     _
+#   (c).-.(c)    (c).-.(c)    (c).-.(c)    (c).-.(c)    (c).-.(c)    (c).-.(c)
+#    / ._. \      / ._. \      / ._. \      / ._. \      / ._. \      / ._. \
+#  __\( Y )/__  __\( Y )/__  __\( Y )/__  __\( Y )/__  __\( Y )/__  __\( Y )/__
+# (_.-/'-'\-._)(_.-/'-'\-._)(_.-/'-'\-._)(_.-/'-'\-._)(_.-/'-'\-._)(_.-/'-'\-._)
+#    || O ||      || O ||      || O ||      || O ||      || O ||      || O ||
+#  _.' `-' '._  _.' `-' '._  _.' `-' '._  _.' `-' '._  _.' `-' '._  _.' `-' '._
+# (.-./`-'\.-.)(.-./`-'\.-.)(.-./`-'\.-.)(.-./`-'\.-.)(.-./`-'\.-.)(.-./`-'\.-.)
+#  `-'     `-'  `-'     `-'  `-'     `-'  `-'     `-'  `-'     `-'  `-'     `-'
+
+###########################################################################
+#   Author:  Matt Fletcher                                                #
+#    Class:  ENG101         	                                          #
+#  Helpers:  None                                                         #
+#                                                                         #
+#  Program:  HW10P1                                                       #
+# Due Date:  16 Nov 2017                                                  #
+#                                                                         #
+# Language:  Python 3.5.1                                                 #
+#      IDE:  Python in Terminal                                           #
+#                                                                         #
+# Purpose:   Print the intialism/acronym for a string of words            #
+#   "Bugs":                                                               #
+#   "Undocumented features": None.                                        #
+###########################################################################
+
+
+#HW 10
+
+
+ #Cue for user input
+print('Enter a series of words')
+user_input = input(">>> ")
+
+#Make user input all caps:
+user_input = user_input.title()
+
+
+#Create list for initials
+initials = []
+
+#Set a boolean "after_space" to true for first letter
+#after_space = 1
+
+#Iterate through words
+for i in range(len(user_input)):
+	if user_input[i].isupper():
+		initials.append(user_input[i])
+
+#Print result
+#TODO make work right
+print(initials)
