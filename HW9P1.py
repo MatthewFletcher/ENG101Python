70cde8a864081f553dce862190e811770829a874
MatthewFletcher <bassmattf@gmail.com>
2017-10-13 14:27:53 -0500

    Final working code, may fiddle with winner function

--- HW9P1.py
+++ HW9P1.py
@@ -37,21 +37,48 @@ from random import randint
 t= ['ROCK','PAPER','SCISSOR','SPOCK','LIZARD']
 
 #Set computer play to random choice 
-computer =t[randint(0,len(t)-1)]
+computer =randint(0,len(t)-1)
 
 print("The computer has chosen.")
 
 #Get user choice
 print('What would you like to play?')
+
+#Because I'm lazy and don't want to type every option every time, plus makes it easier to debug
+#Using a for loop to print list of choices
 for i in range(len(t)):
 	print('%i.\t%s'%((i+1),t[i]))
+#Cue for user input
 player_choice = int(input('Enter choice number here>>> ' ))
+
+#Because list is indexed at 0, subtract 1 from the user's choice.
 player = player_choice - 1
 
 #Print out user's choice
 print('You chose %s'%t[player])
 
 
+#Now that user has input their choice, print computer's choice:
+print('Computer chose %s'%t[computer])
+
 #Figure out who actually won
 #TODO make this cleaner
 
+#I know this is absolutely ugly but I can't figure out a cleaner way to approach this 
+#Set difference of player and computer to variable
+determinant = player - computer
+#If idx of player equals computer, declare tie
+if player == computer:
+    print("It's a tie!")
+#If idx of player is 1 greater or 4 less than computer, player wins
+if determinant == 1 or determinant == -4:
+	print('Player wins')
+#If idx of player is 2 greater or 3 less than computer,  computer wins:
+if determinant == 2 or determinant == -3:
+	print('Computer wins')
+#If idx of player is 3 greater or 2 less than computer,  player wins
+if determinant == 3 or determinant == -2:
+	print('Player wins')
+#If idx of player is 4 greater or 1 less than computer, computer wins
+if determinant == 4 or determinant == -1:
+	print('Computer wins')
